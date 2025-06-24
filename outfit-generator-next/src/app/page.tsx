eca97f58ad1df7f1d2188705228617a97830ce70
dvo27 <danielnvo08@gmail.com>
2025-06-23 20:20:59 -0700

    added drop zone for clothes upload and clothes selection after upload

--- outfit-generator-next/src/app/page.tsx
+++ outfit-generator-next/src/app/page.tsx
@@ -14,19 +14,19 @@ export default function Home() {
 
   const onShirtDrop = useCallback((acceptedFiles: File[]) => {
     inputShirts(prev => [...prev, ...acceptedFiles]);
-   }, []);
+  }, []);
 
   const onPantsDrop = useCallback((acceptedFiles: File[]) => {
     inputPants(prev => [...prev, ...acceptedFiles]);
   }, []);
 
   // Shirt Dropzone
-  const { getRootProps: getShirtRootProps, 
-    getInputProps: getShirtInputProps, 
+  const { getRootProps: getShirtRootProps,
+    getInputProps: getShirtInputProps,
     isDragActive: isDragActiveShirt } = useDropzone({
       // Multiple allows user to upload multiple files at once
       multiple: true,
-      
+
       // onDrop is the function that is called when the user drops a file
       onDrop: onShirtDrop,
 
@@ -35,11 +35,11 @@ export default function Home() {
         "image/png": [".png"],
         "image/jpeg": [".jpg", ".jpeg"],
       },
-  });
+    });
 
   // Pants Dropzone
-  const { getRootProps: getPantsRootProps, 
-    getInputProps: getPantsInputProps, 
+  const { getRootProps: getPantsRootProps,
+    getInputProps: getPantsInputProps,
     isDragActive: isDragActivePants } = useDropzone({
       // Multiple allows user to upload multiple files at once
       multiple: true,
@@ -52,9 +52,9 @@ export default function Home() {
         "image/png": [".png"],
         "image/jpeg": [".jpg", ".jpeg"],
       },
-  });
+    });
 
-    return (
+  return (
     <>
       {/* Header */}
       <header className="flex flex-col items-center mb-4">
@@ -65,13 +65,13 @@ export default function Home() {
           By: Daniel Vo
         </h2>
       </header>
-      
+
       {/* Button Container */}
       <div className="button_container flex justify-center mb-4">
         <button id="random_button" onClick={() => {
           console.log("Random");
         }}>
-        <Image src="/button.png" alt="Random" width={100} height={100} />
+          <Image src="/button.png" alt="Random" width={100} height={100} />
         </button>
       </div>
 
@@ -79,26 +79,80 @@ export default function Home() {
       <div className="main_container flex mb-4">
         <div className="left_side w-1/2 border-2 border-red-500 flex flex-col items-center">
           {/* Clothes Upload/Selector Section */}
-          <div className="clothes_selector flex flex-col gap-4">
-            <label htmlFor="shirt_input" className="file-input-label">
-              Upload Shirt
-            </label>
-
-            {/* Shirt Dropzone */}
-            <div {...getShirtRootProps({className: "dropzone p-8 border-2 border-dashed border-blue-300 rounded-md"})}>
-              <input {...getShirtInputProps()} />
-              <p>Drag and drop shirt files here, or click to select files</p>
+          <div className="clothes_selector flex flex-col gap-4 w-full px-4">
+            <div className="shirt_selector">
+              {/* Shirt Dropzone */}
+              {
+                // If no shirts have been uploaded, show dropzone
+                shirts.length === 0 ? (
+                  <>
+                    <label htmlFor="shirt_input" className="file-input-label">
+                      Upload Shirt
+                    </label>
+                    <div {...getShirtRootProps({ className: "dropzone p-8 border-2 border-dashed border-blue-300 rounded-md" })}>
+                      <input {...getShirtInputProps()} />
+                      <p>Drag and drop shirt files here, or click to select files</p>
+                    </div>
+                  </>
+                ) : (
+                  // If shirts have been uploaded, show their previews:
+                  <>
+                    <label htmlFor="shirt_input" className="file-input-label">
+                      Select Shirt
+                    </label>
+                    <div className="h-72 w-full overflow-y-auto">
+                      {/* Grid of shirts */}
+                      <div className="grid grid-cols-3 gap-4">
+                        {
+                          shirts.map((shirt, index) => (
+                            <div key={index}>
+                              <Image src={URL.createObjectURL(shirt)} alt="Shirt" width={100} height={100} />
+                            </div>
+                          ))
+                        }
+                      </div>
+                    </div>
+                  </>
+                )
+              }
             </div>
-
-            {/* Pants Dropzone */}
-            <label htmlFor="pants_input" className="file-input-label">
-              Upload Pants
-            </label>
-            <div {...getPantsRootProps({className: "dropzone p-8 mb-5 border-2 border-dashed border-blue-300 rounded-md"})}>
-              <input {...getPantsInputProps()} />
-              <p>Drag and drop pants files here, or click to select files</p>
+            <div className="pants_selector">
+              {/* Pants Dropzone */}
+              {
+                // If no pants have been uploaded, show dropzone
+                pants.length === 0 ? (
+                  <>
+                    <label htmlFor="pants_input" className="file-input-label">
+                      Upload Pants
+                    </label>
+                    <div {...getPantsRootProps({ className: "dropzone p-8 mb-5 border-2 border-dashed border-blue-300 rounded-md" })}>
+                      <input {...getPantsInputProps()} />
+                      <p>Drag and drop pants files here, or click to select files</p>
+                    </div>
+                  </>
+                ) : (
+                  // If pants have been uploaded, show their previews:
+                  <>
+                    {/* Pants Selector */}
+                    <label htmlFor="pants_input" className="file-input-label">
+                      Select Pants
+                    </label>
+                    <div className="h-72 w-full overflow-y-auto">
+                      {/* Grid of pants */}
+                      <div className="grid grid-cols-3 gap-4">
+                        {
+                          pants.map((pants, index) => (
+                            <div key={index}>
+                              <Image src={URL.createObjectURL(pants)} alt="Pants" width={100} height={100} />
+                            </div>
+                          ))
+                        }
+                      </div>
+                    </div>
+                  </>
+                )
+              }
             </div>
-
           </div>
         </div>
 
