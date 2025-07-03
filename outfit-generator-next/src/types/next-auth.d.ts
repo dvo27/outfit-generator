a9ab6ab97f882d94220e6cca063806ea5012ddd2
dvo27 <danielnvo08@gmail.com>
2025-07-03 16:42:47 -0700

    fix on type augmentation for route.ts session.user.id not being found

--- outfit-generator-next/src/types/next-auth.d.ts
+++ outfit-generator-next/src/types/next-auth.d.ts
@@ -1,11 +1,10 @@
+import { DefaultSession } from "next-auth";
+
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+    } & DefaultSession["user"];
   }
 
   interface User {
